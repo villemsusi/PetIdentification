2f356d307d556aabf34763efea8124f107953708
villemsusi <villem.susi@gmail.com>
2024-01-11 05:46:29 +0200

    Fixed model checkpoint path

--- scripts/identifier.py
+++ scripts/identifier.py
@@ -5,5 +5,5 @@ from object_predict import inference
 
 if __name__ == "__main__":
     image = detect()
-    res = inference("2023-12-07_13-35-07\efficientnet_b0.ra_in1k.pth", image, "cat")
+    res = inference("efficientnet_b0.ra_in1k.pth", image, "cat")
     print(res)
